8abbb5f483a2da732e17f7a7ee7dae0b2c5df496
mehdi <mehdi.orangpour@deriv.com>
2021-12-31 01:03:29 +0800

    unpaired element in odd array

---
+++ OddOccurrencesInArray/OddOccurrencesInArray.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func Solution(A []int) int {
+	len := len(A)
+	result := 0
+	for i := 0; i < len; i++ {
+		result ^= A[i]
+	}
+	return result
+}
+
+
+func main(){
+	fmt.Println(Solution([]int{1,2,4,2,1}) )
+}
