5e96d057d214f66cbfc0016ca998e451ed8be93a
mehdi <mehdi_hsb@yahoo.com>
2022-01-05 00:22:05 +0800

    make it simpler

--- passingCars/passingCars.go
+++ passingCars/passingCars.go
@@ -20,30 +20,6 @@ func Solution(A []int) int {
 	return counter
 }
 
-/*
-func Solution(A []int) int {
-    // write your code in Go 1.4
-    pairs   := 0
-    counter := 0
-    len     := len(A)
-    for i := 0; i <len; i++{
-       // fmt.Println("print outer:", A[i])
-        if A[i] == 0 {
-            counter++
-        }
-        if (counter > 0){
-            if (A[i] == 1){
-                pairs = pairs + counter;
-                if (pairs > 1000000000){
-                        return -1;
-                }
-            }
-        }
-    }
-
-    return pairs
-}
-*/
 func main() {
 	fmt.Println(Solution([]int{0, 1, 0, 1, 1}))
 }
