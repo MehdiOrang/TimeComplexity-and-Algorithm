acf4420abddf2762be6a356f27a8daadcb26fc3a
mehdi <mehdi_hsb@yahoo.com>
2022-01-05 00:21:11 +0800

    make it simpler

--- passingCars/passingCars.go
+++ passingCars/passingCars.go
@@ -3,20 +3,49 @@ package main
 import "fmt"
 
 func Solution(A []int) int {
-	pairs := 0
-	counter := true
-	len := len(A)
-	for i := 0; i < len; i++ {
-		for {
-			if !counter {
-				break
-			}
+	counter := 0
+	incrementVal := 0
+	for i := 0; i < len(A); i++ {
+		if A[i] == 0 {
+			incrementVal++
+		}
+		if A[i] == 1 {
+			counter = counter + incrementVal
+		}
+		if counter > 1000000000 {
+			return -1
 		}
 	}
 
-	return pairs
+	return counter
 }
 
-func main() {
-	fmt.Println(Solution([]int{1, 2, 4, 2, 1}))
+/*
+func Solution(A []int) int {
+    // write your code in Go 1.4
+    pairs   := 0
+    counter := 0
+    len     := len(A)
+    for i := 0; i <len; i++{
+       // fmt.Println("print outer:", A[i])
+        if A[i] == 0 {
+            counter++
+        }
+        if (counter > 0){
+            if (A[i] == 1){
+                pairs = pairs + counter;
+                if (pairs > 1000000000){
+                        return -1;
+                }
+            }
+        }
+    }
+
+    return pairs
 }
+*/
+func main() {
+	fmt.Println(Solution([]int{0, 1, 0, 1, 1}))
+}
+
+//https://app.codility.com/demo/results/trainingRZSGPY-UJQ/
