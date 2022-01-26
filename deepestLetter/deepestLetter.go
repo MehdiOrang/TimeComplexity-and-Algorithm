f336b6c65dfa731c29bc42c803f98044d24ced4e
mehdi <mehdi_hsb@yahoo.com>
2022-01-26 23:53:39 +0800

    wip

--- deepestLetter/deepestLetter.go
+++ deepestLetter/deepestLetter.go
@@ -1,44 +1,62 @@
-package main
+package tests
 
 import (
-    "fmt"
-    "import testing"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func getDeepestLetter(s string) rune {
-  currentMax := 0
-  mainMax := 0
-  result := '?'
-  length := len(s)
-  for i := 0; i< length; i++{
-    if s[i] == '('{
-	currentMax++
-	if currentMax > mainMax{
-	  mainMax = currentMax
-          result =rune(s[i+1])
+	currentMax := 0
+	mainMax := 0
+	result := '?'
+	length := len(s)
+	for i := 0; i < length; i++ {
+		if s[i] == '(' {
+			currentMax++
+			if currentMax > mainMax {
+				mainMax = currentMax
+				result = rune(s[i+1])
+			}
+		} else if s[i] == ')' {
+			if currentMax > 0 {
+				currentMax--
+			} else {
+				return '?'
+			}
+		}
 	}
-    }else if  s[i] == ')'{
-	if currentMax > 0 {
-	  currentMax--
-	}else{
-	  return '?'
+
+	if currentMax != 0 {
+		return '?'
 	}
-    }
-  }
 
-  if currentMax != 0 {
-	return '?'
-  }
-
-  return result
+	return result
 
 }
 
-func main(){
-  fmt.Printf("%c\n",getDeepestLetter("a(b)c"))
-  fmt.Printf("%c\n",getDeepestLetter("((A)(b)c"))
-  fmt.Printf("%c\n",getDeepestLetter("(8)"))
-  fmt.Printf("%c\n",getDeepestLetter("(!)"))
-  fmt.Printf("%c\n",getDeepestLetter("((a))(((M)))(c)(D)(e)(((f))(((G))))h(i)"))
-  fmt.Printf("%c\n",getDeepestLetter("( p((q)) ((s)t) )"))
+type letterTestCase struct {
+	input    string
+	expected rune
+}
+
+func TestGetDeepestLetter(t *testing.T) {
+	testCases := []letterTestCase{
+		{input: "a(b)c", expected: 'b'},
+		{input: "((a))(((M)))(c)(D)(e)(((f))(((G))))h(i)", expected: 'G'}, //changed
+		{input: "((A)(b)c", expected: '?'},                                //changed
+		{input: "(a)((G)c)", expected: 'G'},                               // changed
+		{input: "(8)", expected: '8'},                                     //changed
+		{input: "(!)", expected: '!'},                                     //changed
+	}
+
+	for _, test := range testCases {
+		t.Run(test.input, func(t *testing.T) {
+			actual := getDeepestLetter(test.input)
+			if !cmp.Equal(test.expected, actual) {
+				t.Log(cmp.Diff(test.expected, actual))
+				t.Fail()
+			}
+		})
+	}
 }
